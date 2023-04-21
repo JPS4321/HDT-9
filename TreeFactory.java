9411b348090cb6a4049e580f888c15210daa5560
JPS4321 <104738904+JPS4321@users.noreply.github.com>
2023-04-21 12:51:28 -0600

    a

---
+++ TreeFactory.java
@@ -0,0 +1,19 @@
+public class TreeFactory {
+
+    public static <K extends Comparable<K>, V> Tree<K, V> createTree(String treeType) {
+        if (treeType == null) {
+            throw new IllegalArgumentException("Tree type must not be null");
+        }
+
+        switch (treeType) {
+            case "avl":
+                return new AVLTree<>();
+            case "rbt":
+                return new RedBlackTree<>();
+            case "hashmap":
+                return new HashMapTree<>();
+            default:
+                throw new IllegalArgumentException("Invalid tree type");
+        }
+    }
+}
