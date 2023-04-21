56a6ddb3f73e877d3d5bd96f0244428126520014
JPS4321 <104738904+JPS4321@users.noreply.github.com>
2023-04-21 12:51:54 -0600

    d

---
+++ Reader.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+public class Reader {
+    ArrayList<String> lineas = new ArrayList<String>();
+    /**
+     * @param Arch Es la direccion en donde esta el archivo que se debe leer
+     * @author Juan Pablo Solis
+     * @return String
+     */
+    public void Leer(String Arch){
+        try (BufferedReader br = new BufferedReader(new FileReader(Arch))) {
+            String lineaActual;
+            while ((lineaActual = br.readLine()) != null) {
+                lineas.add(lineaActual);
+            }//Por si surge error leyendo el archivo, para que no salga error en el codigo
+        } catch (IOException e) {
+            System.out.println("Ocurrió un error al leer el archivo: " + e.getMessage());
+        }
+    }
+
+
+
+}
